e2e08ca125bf4e4bb5a3957d6d198d466c26ed7f
Vladou73 <vladimir.nafissi@essec.edu>
2021-02-23 19:36:59 +0100

    added html footer/header components with jquery

---
+++ js/components.js
@@ -0,0 +1,6 @@
+$(function(){
+    console.log('jQuery components function ready');
+    $("#header").load("./components/header.html"); 
+    $("#header-sticky").load("./components/header_sticky.html"); 
+    $("#footer").load("./components/footer.html"); 
+  });
